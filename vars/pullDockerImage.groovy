d88fd03194679891d08dc9f5138256eefc2266d0
Saikat <saikat@sporoutsai.com>
2024-12-31 01:04:36 +0530

    try imageURL assing first

--- vars/pullDockerImage.groovy
+++ vars/pullDockerImage.groovy
@@ -1,7 +1,8 @@
 def call(String registryURL, String imageName) {
     script {
+        def imageURL = "${registryURL}/${imageName}:latest"
         sh """
-        ssh -i ${env.SSH_KEY} -t ${env.SSH_USER}@${env.SSH_HOST} 'docker pull ${registryUrl}/${imageName}:latest'
+        ssh -i ${env.SSH_KEY} -t ${env.SSH_USER}@${env.SSH_HOST} 'docker pull ${imageURL}'
         """
     }
 }
