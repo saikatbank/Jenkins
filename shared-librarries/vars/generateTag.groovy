7d1d0e7bc0a055ab3f3c590403659325eaacb4d2
Saikat <saikat@sporoutsai.com>
2024-12-30 17:08:07 +0530

    load tags

---
+++ shared-librarries/vars/generateTag.groovy
@@ -0,0 +1,15 @@
+def call() {
+    script {
+        def tag = "5.0.1" //Default tag
+        if (fileExists('tag.txt')) {
+            def lastTag = readFile('tag.txt').trim()
+            def parts = lastTag.tokenize('.')
+            def newPatch = (parts[-1] as Integer) + 1
+            tag = "${parts[0]}.${parts[1]}.${newPatch}"
+        }
+        writeFile file: 'tag.txt', text: tag  //Write the new tag to file
+
+        echo "Generated tag: ${tag}"
+        return tag
+    }
+}
