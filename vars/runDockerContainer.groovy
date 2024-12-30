b3aacf8f9e2ea323ee84f5adbef2f6a0fad999e2
Saikat <saikat@sporoutsai.com>
2024-12-31 01:06:26 +0530

    fix imageURL issue

--- vars/runDockerContainer.groovy
+++ vars/runDockerContainer.groovy
@@ -1,8 +1,9 @@
 def call(String containerName, String portMapping, String environmentVariable, String registryUrl, String imageName) {
     script {
+        def imageURL = "${registryUrl}/${imageName}:latest"
         sh """
         ssh -i ${env.SSH_KEY} -t ${env.SSH_USER}@${env.SSH_HOST} '
-        docker run -d -p ${portMapping} -e "ENVIRONMENT_VARIABLE_VALUE=${environmentVariable}" --restart=always --name=${containerName} ${registryUrl}/${imageName}:latest
+        docker run -d -p ${portMapping} -e "ENVIRONMENT_VARIABLE_VALUE=${environmentVariable}" --restart=always --name=${containerName} ${imageURL}
         '
         """
     }
