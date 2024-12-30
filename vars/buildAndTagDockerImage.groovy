59240159e0c50b6685cf0267a1525c58407ea023
Saikat <saikat@sporoutsai.com>
2024-12-30 18:37:04 +0530

    buildAndTagDockerImage changed from single quotes to double quotes

--- vars/buildAndTagDockerImage.groovy
+++ vars/buildAndTagDockerImage.groovy
@@ -1,9 +1,9 @@
 def call(String imageName, String registryUrl, String tag) {
     script {
-        sh '''
+        sh """
         docker build -t ${registryUrl}/${imageName}:${tag} .
         docker tag ${registryUrl}/${imageName}:${tag} ${registryUrl}/${imageName}:latest
-        '''
+        """
 
         echo "Docker image built and tagged as ${registryUrl}/${imageName}:${tag} and ${registryUrl}/${imageName}:latest"
 
