0421b734bcc7ea9e0e5946c1c92693ead007030d
Saikat <saikat@sporoutsai.com>
2024-12-31 19:54:06 +0530

    loadProjectConfig added

---
+++ vars/loadProjectConfig.groovy
@@ -0,0 +1,23 @@
+import groovy.json.jsonSlurper
+
+def call(String key) {
+    // Path to the JSON file
+    def jsonFilePath = '/home/devopsuser/testProjectConfig.json' // Replace with actual path
+    
+    // Read the JSON file
+    def jsonFile = new File(jsonFilePath)
+    if (!jsonFile.exists()) {
+        error "JSON file not found at ${jsonFilePath}"
+    }
+
+    // Parse the JSON content
+    def jsonContent = new JsonSlurper().parse(jsonFile)
+
+    // Retrieve the value corresponding to the key
+    def value = jsonContent[key]
+    if (!value) {
+        error "Key '${key}' not found in the JSON configuration"
+    }
+
+    return value
+}
